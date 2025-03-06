4fa8fb5d60678a008280bbc612aa47159c635136
uday <udaysubba2004@gmail.com>
2025-03-06 21:29:18 +0530

    add staff service

--- app/services/staff_service.py
+++ app/services/staff_service.py
@@ -1,6 +1,7 @@
 from app.db.models.staff_model import Staff
 from app.db.session import SessionDep
 from fastapi import HTTPException
+from sqlalchemy import select
 
 def get_staff(staff_id: int, session: SessionDep):
     staff_in_db = None
@@ -13,3 +14,28 @@ def get_staff(staff_id: int, session: SessionDep):
         raise HTTPException(status_code=404, detail="Staff not found!")
 
     return staff_in_db
+
+
+def list_staffs(session: SessionDep):
+    try:
+        statement = select(Staff)
+        result =  session.execute(statement).mappings().all()
+        staffs = []
+        for row in result:
+            staffs.append(row.Staff.__dict__)
+
+        return staffs
+    except Exception as e:
+        print(e)
+
+def list_staffs_by_center(center_id: int, session: SessionDep):
+    try:
+        statement = select(Staff).where(Staff.staff_center_id == center_id)
+        result =  session.execute(statement).mappings().all()
+        staffs = []
+        for row in result:
+            staffs.append(row.Staff.__dict__)
+
+        return staffs
+    except Exception as e:
+        print(e)
