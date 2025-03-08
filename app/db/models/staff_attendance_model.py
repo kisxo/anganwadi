f0e9a8345c5ae315076557a39250ceb011c06b41
uday <udaysubba2004@gmail.com>
2025-03-08 20:56:18 +0530

    update attendance models

--- app/db/models/staff_attendance_model.py
+++ app/db/models/staff_attendance_model.py
@@ -1,10 +1,14 @@
 from app.db.database import Base
-from sqlalchemy import TIMESTAMP, func, ForeignKey
+from sqlalchemy import TIMESTAMP, func, ForeignKey, Enum as SqlEnum, DATE
 from sqlalchemy.orm import Mapped, mapped_column
+from app.db.schemas.attendance import AttendanceModeChoice
 
 class StaffAttendance(Base):
     __tablename__ = "staff_attendance"
 
     attendance_id: Mapped[int] = mapped_column(primary_key=True)
     attendance_staff_id: Mapped[int] = mapped_column(ForeignKey("staffs.staff_id"))
-    attendance_date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    attendance_center_id: Mapped[int] = mapped_column(ForeignKey("anganwadi_centers.center_id"))
+    attendance_mode: Mapped[SqlEnum] = mapped_column(SqlEnum(AttendanceModeChoice))
+    attendance_date: Mapped[DATE] = mapped_column(DATE, server_default=func.now())
+    attendance_create_date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
--- app/db/models/student_attendance_model.py
+++ app/db/models/student_attendance_model.py
@@ -1,10 +1,14 @@
 from app.db.database import Base
-from sqlalchemy import TIMESTAMP, func, ForeignKey
+from sqlalchemy import TIMESTAMP, func, ForeignKey, Enum as SqlEnum, DATE
 from sqlalchemy.orm import Mapped, mapped_column
+from app.db.schemas.attendance import AttendanceModeChoice
 
 class StudentAttendance(Base):
     __tablename__ = "student_attendance"
 
     attendance_id: Mapped[int] = mapped_column(primary_key=True)
     attendance_student_id: Mapped[int] = mapped_column(ForeignKey("students.student_id"))
-    attendance_date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    attendance_center_id: Mapped[int] = mapped_column(ForeignKey("anganwadi_centers.center_id"))
+    attendance_mode: Mapped[SqlEnum] = mapped_column(SqlEnum(AttendanceModeChoice))
+    attendance_date: Mapped[DATE] = mapped_column(DATE, server_default=func.now())
+    attendance_create_date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
