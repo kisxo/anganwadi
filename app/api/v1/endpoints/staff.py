5b45e454aa87b733e720bcb2b785c0a0ae36b749
uday <udaysubba2004@gmail.com>
2025-03-08 09:41:33 +0530

    add staff create route

--- app/api/v1/endpoints/staff.py
+++ app/api/v1/endpoints/staff.py
@@ -4,7 +4,12 @@ from app.db.session import SessionDep
 from app.core.security import authx_security, auth_scheme
 from authx import TokenPayload
 from app.services import staff_service
-from app.db.schemas.staff import StaffPublic, StaffCreate
+from app.db.schemas.staff import StaffPublic, StaffCreate, Staff
+from app.db.models import staff_model
+from app.services.image import save_image
+from app.services.face_id import generate_face_id
+from app.db.schemas.face_id import FaceID
+from app.core.security import hash_password
 
 router = APIRouter()
 
@@ -49,65 +54,30 @@ async def create_staff(
     if payload.user_type not in ["officer", "admin"]:
         raise HTTPException(status_code=400, detail="Does not have permission to create staff!")
     # result = staff_service.get_staff(staff_id=staff_id,session=session)
-    return {'data': "result"}
-
- = APIRouter()
-
-@router.post("/",
-
-)
-async def create_student(
-    ,
-
-
-):
-    if payload.user_type == "staff":
-        current_user = staff_service.get_staff(payload.user_id, session)
-        input_data.student_center_id = current_user.staff_center_id
-    elif payload.user_type == "admin":
-        current_user = admin_service.get_admin(payload.user_id, session)
-    else:
-        raise HTTPException(status_code=403, detail="Forbidden !")
 
     try:
-        # Format: center_id + '-' + uuid
-        unique_image_id = f"{input_data.student_center_id}-{uuid4()}"
 
-        with open(f"media/images/students/{unique_image_id}", "bw") as image_file:
-            contents = await input_data.student_image_file.read()
-            image_file.write(contents)
+        unique_image_id = save_image(input_data.staff_image_file, "staffs", input_data.staff_center_id)
 
-        face_id : FaceID = generate_face_id(image_group="students", image_id=unique_image_id)
+        face_id: FaceID = generate_face_id(image_group="staffs", image_id=unique_image_id)
         face_id_status = False
         if face_id:
             face_id_status = True
 
-        validated_student = Student(
+        validated_staff = Staff(
             **input_data.model_dump(),
-            student_image=unique_image_id,
-            student_face_id= face_id.model_dump_json(),
-            student_face_id_status= face_id_status
+            staff_image=unique_image_id,
+            staff_face_id=face_id.model_dump_json(),
+            staff_face_id_status=face_id_status,
+            staff_hashed_mpin= hash_password(input_data.staff_mpin)
         )
-        new_student = student_model.Student(**validated_student.model_dump())
+        new_staff = staff_model.Staff(**validated_staff.model_dump())
 
-        session.add(new_student)
+        session.add(new_staff)
         session.commit()
-        session.refresh(new_student)
+        session.refresh(new_staff)
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail="Something went wrong!")
 
-    return new_student
-
-@router.get("/",
-    dependencies=[Depends(authx_security.access_token_required), Depends(auth_scheme)]
-)
-async def list_students(
-    session: SessionDep,
-    payload: TokenPayload = Depends(authx_security.access_token_required)
-):
-    if payload.user_type == "staff":
-        current_user = staff_service.get_staff(payload.user_id, session)
-        center_id = current_user.staff_center_id
-        result = student_service.list_students_by_center(center_id=center_id, session=session)
-        return {'data': result}
+    return new_staff
