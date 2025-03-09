8041f13bb880c1c966c020b2c9a3a2484ea3de38
uday <udaysubba2004@gmail.com>
2025-03-10 00:50:23 +0530

    update ration schema

--- app/db/schemas/ration.py
+++ app/db/schemas/ration.py
@@ -1,11 +1,8 @@
 from pydantic import BaseModel
-from pydantic.types import date
-
 
 class RationItem(BaseModel):
     item: str
     quantity: str
 
 class RationDetail(BaseModel):
-    last_restock_date: date
-    last_restock_detail: list[RationItem]
+    detail: list[RationItem]
