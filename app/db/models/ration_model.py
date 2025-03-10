15e9334f8a7141f83041ff43d95d1b9507f1d044
uday <udaysubba2004@gmail.com>
2025-03-10 17:55:07 +0530

    update ration model

--- app/db/models/ration_model.py
+++ app/db/models/ration_model.py
@@ -8,5 +8,6 @@ class Rations(Base):
 
     ration_id: Mapped[int] = mapped_column(primary_key=True)
     ration_center_id: Mapped[int] = mapped_column(ForeignKey("anganwadi_centers.center_id"))
-    ration_data: Mapped[[JSON]] = mapped_column(JSON())
+    ration_note: Mapped[str] = mapped_column(String())
+    ration_items: Mapped[[JSON]] = mapped_column(JSON())
     ration_created_date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
